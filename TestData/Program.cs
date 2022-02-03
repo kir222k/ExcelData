9fdf50afcc2aa508f00fa14fdaf3baef4e70af6d
kir222k <u.k.send@gmail.com>
2022-02-03 11:32:35 +0300

    + далее - на ветку с другим способом доступа к файлам ексль

--- TestData/Program.cs
+++ TestData/Program.cs
@@ -12,12 +12,21 @@ namespace TestData
     {
         static void Main(string[] args)
         {
-            var pr = ExcelBook.ExcelBookTest();
-            Console.WriteLine(pr);
+            try
+            {
 
-            int x = 10; int y = 2; string range = "C16";
-            var Arr = ExcelBook.GetArrayBasedCell(
-                ExcelBook.GetExcelSheet(Const.FileXlsName, Const.ExcelWorksheet), x, y, range);
+            // var pr = ExcelBook.ExcelBookTest();
+            // Console.WriteLine(pr);
+
+            //Excel.Worksheet ws;
+            //ws = ExcelBook.GetExcelSheet(Const.FileXlsName, Const.ExcelWorksheet);
+            //Console.WriteLine("\nКнига получена");
+            Console.WriteLine("\nПолучение данных");
+            ExcelBook ExBook = new ExcelBook();
+
+            int x = 150; int y = 80; string range = "A1";
+            var Arr = ExBook.GetArrayBasedCell(Const.FileXlsName, Const.ExcelWorksheet , x, y, range);
+            Console.WriteLine("\nМассив получен");
 
             for (int i = 0; i < x; i++)
             {
@@ -27,9 +36,42 @@ namespace TestData
                 }
                 Console.WriteLine("\n");
             }
-        
+            // ws.Application.Quit();
 
-            Console.ReadKey();
+            /*
+
+            // 3. Получим массив 50*50 значений  от А1, поищем там ячейку с текстом "Блок"
+            int x1 = x; int y1 = y; string range1 = range;
+            //ws = ExcelBook.GetExcelSheet(Const.FileXlsName, Const.ExcelWorksheet);
+            //var ArrForBlock = ExcelBook.GetArrayBasedCell(ws, x1, y1, range1);
+            var ArrForBlock = Arr;
+            for (int i = 0; i < x1; i++)
+            {
+                for (int j = 0; j < y1; j++)
+                {
+                    // Console.Write(Arr[i, j].ToString() + " ");
+                    if (ArrForBlock[i, j] == "Блок")
+                    {
+                        // кортеж с данными (строка, столбец) , где сидит слово "Блок"
+                        var rangeBlock = (i, j); // столбец, в кот. будут имена блоков найден
+                        Console.WriteLine("\nСЛОВО: Блок =>" + "Строка: "  + rangeBlock.i + " Столбец: " + rangeBlock.j);
+                        //Console.WriteLine("\nСЛОВО: Блок =>" +  ws.Range();
+                        break;
+                    }
+                }
+            }
+            */
+
+            // ws.Application.Quit();
+
+            Console.WriteLine("\n\nДля завершения работы нажмите л. кл.");
+            Console.ReadLine();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
     }
 }
